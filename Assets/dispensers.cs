f3569c2bf5aa76fa2c33c32d25eccffd5264c612
project-AFRO <78296190+project-AFRO@users.noreply.github.com>
2022-04-02 19:40:07 +0200

    dispenser types and names

--- Assets/dispensers.cs
+++ Assets/dispensers.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class dispensers : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private byte dispenserType;
+    [SerializeField] private string[] dispenserName;
     void Start()
     {
-        
+        dispenserName[3];
+        dispenserName = new string["latte", "espresso", "black"];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+     if (dispenserType == 0)
+        {
+
+        }
         
+        
+           
+    }
+
+    public byte GetDispansertype()
+    {
+        return dispenserType;
     }
 }
