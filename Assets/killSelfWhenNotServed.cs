de6bc7f8a1448144d83a889beb094af4817faab4
project-AFRO <78296190+project-AFRO@users.noreply.github.com>
2022-04-05 00:57:12 +0200

    .....

--- Assets/killSelfWhenNotServed.cs
+++ Assets/killSelfWhenNotServed.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class killSelfWhenNotServed : MonoBehaviour
 {
     float timer = 0;
-    public int time_to_leave = 15;
+    public int time_to_leave = 30;
+    public doSpawnCostumer costumerScript;
+    public GameObject door;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        costumerScript = door.GetComponent<doSpawnCostumer>();
     }
     
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class killSelfWhenNotServed : MonoBehaviour
         {
             // do something here and then destroy self
             Destroy(gameObject);
+            costumerScript.setNumOfCostumers(costumerScript.getNumOfCostumers()-1);
         }
     }
 }
