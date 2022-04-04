fdbf317002b4aeb14ce5f2d06993035eb29015f2
project-AFRO <78296190+project-AFRO@users.noreply.github.com>
2022-04-05 01:44:49 +0200

    ..

---
+++ Assets/Scenes/applicationQuit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class applicationQuit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
